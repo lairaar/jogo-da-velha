a8ce244e6bf401496c930200b3391b3c56176e17
lairaar <lairaarruda@gmail.com>
2017-12-07 10:52:26 -0200

    Update index.java

--- js/index.java
+++ js/index.java
@@ -1,6 +1,7 @@
 var contX = 0;
 var contO = 0;
-vez = 1;
+var cont = 0;
+var vez = 1;
   function apaga() {
 			var top1 = document.getElementById("1");
 			var top2 = document.getElementById("2");
@@ -20,10 +21,13 @@ vez = 1;
 			top7.value = "";
 			top8.value = "";
 			top9.value = "";
+			document.getElementById("O").disabled = false;
+			document.getElementById("X").disabled = false; 
+			cont=0;
 		}
 		
-  function bloco(bloco) {
-    var selbloco = document.getElementById(bloco);
+  function bloco(campo) {
+    var selbloco = document.getElementById(campo);
     var top1 = document.getElementById("1");
 		var top2 = document.getElementById("2");
 		var top3 = document.getElementById("3");
@@ -51,7 +55,7 @@ vez = 1;
 			 }
   }
      
-  function confirma() {
+  function comfirma() {
     var jog1 = document.getElementById("jog1");
     var jog2 = document.getElementById("jog2");
     var top1 = document.getElementById("1");
@@ -69,12 +73,11 @@ vez = 1;
     //////////////////////////////// x /////////////////////////////////// 
       //horizontal//  
   if  (((top1.value == "X") && (top2.value == "X") && (top3.value == "X"))  || 
-       ((top7.value == "X") && (top7.value == "X") && (top9.value == "X"))  ||
+       ((top7.value == "X") && (top8.value == "X") && (top9.value == "X"))  ||
        ((top4.value == "X") && (top5.value == "X") && (top6.value == "X"))  ||
       //diagonal//
         ((top3.value == "X") && (top5.value == "X") && (top7.value == "X")) ||
         ((top1.value == "X") && (top5.value == "X") && (top9.value == "X")) ||
-        
       //vertical//
         ((top1.value == "X") && (top4.value == "X") && (top7.value == "X")) || 
         ((top2.value == "X") && (top5.value == "X") && (top8.value == "X")) ||
@@ -84,39 +87,43 @@ vez = 1;
 				alert(X.value + " " + "ganhou!")
           }
 				else {
-				alert("jog1 ganhou!")
-          } 
+				alert("jog1 ganhou!");
+        cont = 1;  
+				  
+				} 
       }
   //////////////////////////////// bolinha ///////////////////////////////////
       //horizontal//  
   if  (((top1.value == "O") && (top2.value == "O") && (top3.value == "O"))  ||
-       ((top7.value == "O") && (top7.value == "O") && (top9.value == "O"))  ||
+       ((top7.value == "O") && (top8.value == "O") && (top9.value == "O"))  ||
        ((top4.value == "O") && (top5.value == "O") && (top6.value == "O"))  ||
       //diagonal//
         ((top3.value == "O") && (top5.value == "O") && (top7.value == "O")) ||
-        ((top1.value == "O") && (top5.value == "O") && (top7.value == "O")) ||
+        ((top1.value == "O") && (top5.value == "O") && (top9.value == "O")) ||
       //vertical//
         ((top1.value == "O") && (top4.value == "O") && (top7.value == "O")) ||
         ((top2.value == "O") && (top5.value == "O") && (top8.value == "O")) ||
         ((top3.value == "O") && (top6.value == "O") && (top9.value == "O")))
         {
           if (O.value !== "") {
-				alert(O.value + " " + "ganhou!")
+				alert(O.value + " " + "ganhou!");
           }
-				else {
-				alert("jog2 ganhou!")
-         }
+          else {
+            alert("jog2 ganhou!");
+            cont = 0;
+          }
         }
-      if (((top1.value == "X")) && ((top2.value == "O")||
-      (top2.vatopue == "X")) && ((top3.value == "O")||
-      (top3.value == "X")) && ((top4.value == "O")||
-      (top4.value == "X")) && ((top5.value == "O")||
-      (top5.value == "X")) && ((top6.value == "O")||
-      (top6.value == "X")) && ((top7.value == "O")||
-      (top7.value == "X")) && ((top8.value == "O")||
-      (top8.value == "X")) && ((top9.value == "O")||
-      (top9.value == "X"))) {
-				alert("Deu velha!");
-      }
+        velha(top1, top2, top3, top4, top5, top6, top7, top8, top9);
   }
+          
   
+    function velha(top1, top2, top3, top4, top5, top6, top7, top8, top9){
+      if (cont == 9) {
+    alert("velha!!") ; 
+  
+      }
+     
+	if ((top1.value !== "") || (top2.value !== "") || (top3.value !== "") || (top4.value !== "") || (top5.value !== "") || (top6.value !== "") || (top7.value !== "") || (top8.value !== "") || (top9.value !== "")){
+	  cont += 1;
+        }
+     }
